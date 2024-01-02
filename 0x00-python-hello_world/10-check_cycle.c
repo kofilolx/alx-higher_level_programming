533655ebe51de8a435d59daed724988fd2971ee8
kofilolx <kofyamando@gmail.com>
2024-01-02 06:49:18 -0500

    C: Linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,6 +4,7 @@
  * check_cycle - checks if a singly linked list has a cycle in it
  * @list: pointer to the head of the linked list
  * Return: 0 if there is no cycle, 1 if there is a cycle
+*/
 
 int check_cycle(listint_t *list)
 {
