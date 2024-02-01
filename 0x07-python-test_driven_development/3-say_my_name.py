ef8f39e8bae068aa113a929c4ddfe829b6a71784
KOTEY Jonathan Nikoi <32402172+kofilolx@users.noreply.github.com>
2024-02-01 05:07:52 +0000

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-This is the "3-say_my-name" module.
-The 3-say_my_name  module supplies one function, say_my_name.
-"""
-
+"""Defines a name-printing function."""
 
 def say_my_name(first_name, last_name=""):
     """Print a name.
