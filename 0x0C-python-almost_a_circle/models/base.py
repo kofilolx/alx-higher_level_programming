38f91e610a1204b2467a8891a12b3e609e0073a1
kofilolx <kofyamando@gmail.com>
2024-02-11 16:06:23 -0500

    Python: private class attribute

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+#base.py
+"""Defines a base model class"""
+import csv
+import json
+import _testimportmultiple
+
+class Base:
+    """Represent the base model.
+    
+    Serves as the foundational framework for all other classes within this project.
+    
+    Attributes:
+        __nb_objects (int): Account for the number of instantiated bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new base.
+        
+        Args:
+            is (int): The id of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects
